a76773f9edda4bd64faafd3fa6506e1f41dc37cb
Sambauel <151626951+Sambauel@users.noreply.github.com>
2023-11-23 20:19:46 +0100

    Update BanensBeste.py

--- BanensBeste.py
+++ BanensBeste.py
@@ -34,7 +34,7 @@ with open(kamper) as fil:
     x = fil.readline()
     x = x.split(',')
     ordbok = {}
-    a = x.index('player_of_match')
+    BanensBeste = x.index('player_of_match')
     for x in fil:
         x = x.split(',')
         try:
@@ -46,6 +46,6 @@ with open(kamper) as fil:
            
             ordbok[x[1]]['Banens beste'] = {}
         try:
-            ordbok[x[1]]['Banens beste'][x[a]] += 1
+            ordbok[x[1]]['Banens beste'][x[BanensBeste]] += 1
         except:
-            ordbok[x[1]]['Banens beste'][x[a]] = 1
+            ordbok[x[1]]['Banens beste'][x[BanensBeste]] = 1
