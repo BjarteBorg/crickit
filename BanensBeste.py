ac1a0897e1f83348a5f9a8e156cc153819402eb0
BjarteBorg <151626079+BjarteBorg@users.noreply.github.com>
2023-12-01 15:20:52 +0100

    Update BanensBeste.py

--- BanensBeste.py
+++ BanensBeste.py
@@ -1,19 +1,22 @@
 import matplotlib.pyplot as plt
 kamper = 'matches.csv'
- 
- 
-def BanensBestePlot(Start, Antall):
-    Aar = []
-    for i in range(Antall):
-        Aar.append(Start+i)
-    global liste
-    global liste2
-    
+
+
+
+def BanensBestePlot(*Aar):
+    Aar = list(Aar)
+    Aar = sorted(Aar)
+    antall = Aar[-1] - Aar[0]
+    for i in range(1,antall):
+        ar = Aar[0] + i
+        Aar.append(ar)
+    Aar = sorted(Aar)
     liste = []
     liste2 = []
     for x in ordbok[str(Aar[0])]['Banens beste'].items():
         liste.append([x[0], x[1]])
         liste2.append(x[0]) 
+    
     for i in range(len(Aar)-1):
         for y in ordbok[str(Aar[i+1])]['Banens beste'].items():
             if y[0] in liste2:
@@ -21,16 +24,21 @@ def BanensBestePlot(Start, Antall):
             else:
                 liste.append([y[0], y[1]])
                 liste2.append(y[0])
-
+    
+    for a in liste2:
+        if liste2.count(a) > 1:
+            print('Flere enn en')
+                  
     liste = sorted(liste, key=lambda x: -x[-1])
     a = liste[10][-1]
     b = sum(x[-1] == a for x in liste)
     c = sum(x[-1] == a for x in liste[:10])
-
+       
+    
     plt.subplots(1,1, figsize = (6, 8))
     plt.table(cellText=liste[:10], loc = 'center', bbox=[0, 0, 1, 1])
     plt.axis('off')
-    plt.title('De 10 spillerne med flest banens beste kåringer mellom ' + str(Aar[0]) + " og " + str(Aar[len(Aar)-1]))
+    plt.title('De 10 spillerne med flest banens beste kåringer fra ' + str(Aar[0]) + " til " + str(Aar[-1]))
     if b-c == 1:
         plt.text(0, -0.04, 'Det er en spiller til med ' + str(a) + ' kåringer.')
         plt.text(0, -0.08, 'Totalt blir ' + str(len(liste)) + ' spillere kåret til banens beste.')
@@ -39,9 +47,10 @@ def BanensBestePlot(Start, Antall):
         plt.text(0, -0.08, 'Totalt blir ' + str(len(liste)) + ' spillere kåret til banens beste.')
     else:
         plt.text(0, -0.08, 'Totalt blir ' + str(len(liste)) + ' spillere kåret til banens beste.')
+    
 
- 
- 
+
+
 with open(kamper) as fil:
     x = fil.readline()
     x = x.split(',')
@@ -55,6 +64,7 @@ with open(kamper) as fil:
             ordbok[x[1]] = {}
             ordbok[x[1]]['Aar'] = x[1]
             ordbok[x[1]]['AntallKamper'] = 1
+           
             ordbok[x[1]]['Banens beste'] = {}
         try:
             if not x[BanensBeste] == '':
@@ -62,5 +72,7 @@ with open(kamper) as fil:
         except:
             if not x[BanensBeste] == '':
                 ordbok[x[1]]['Banens beste'][x[BanensBeste]] = 1
+    
                 
-BanensBestePlot(2008, 2) #startår og antall år
+BanensBestePlot(2008, 2017)
+
